b28e2e36c0ba940c10419179bafd5bfcef03d49b
Adrysson <adrysson@doity.com.br>
2021-08-01 22:45:17 -0300

    Created abstract repository testing class

--- tests/Repositories/CompanyRepositoryTest.php
+++ tests/Repositories/CompanyRepositoryTest.php
@@ -2,107 +2,30 @@
 
 use App\Models\Company;
 use App\Repositories\Contracts\CompanyRepositoryInterface;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Database\QueryException;
-use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Repositories\Contracts\MainRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
 
-class CompanyRepositoryTest extends TestCase
+require_once 'RepositoryTest.php';
+
+class CompanyRepositoryTest extends RepositoryTest
 {
-    use DatabaseTransactions;
+    protected string $table = 'companies';
 
-    private CompanyRepositoryInterface $repository;
-
-    public function setUp(): void
+    protected function getRepository(): MainRepositoryInterface
     {
-        parent::setUp();
-
-        $this->repository = app(CompanyRepositoryInterface::class);
+        return app(CompanyRepositoryInterface::class);
     }
 
-    public function paginateProvider()
+    protected function getModel(): Model
     {
-        return [
-            '20-per-page' => [20],
-            '30-per-page' => [30],
-            '40-per-page' => [40],
-        ];
+        return new Company;
     }
 
-    /**
-     * @dataProvider paginateProvider
-     */
-    public function testPaginate(int $perPage)
-    {
-        Company::factory()->count(30)->create();
+    protected array $createData = [
+        'name' => 'Nubank'
+    ];
 
-        $paginator = Company::paginate($perPage);
-
-        $repositoryPaginator = $this->repository->paginate($perPage);
-
-        $this->assertEquals($paginator->toArray(), $repositoryPaginator->toArray());
-    }
-
-    public function testGetById()
-    {
-        $company = $this->repository->getById();
-        $this->assertEquals($company, []);
-
-        $companyCreated = Company::factory()->create();
-
-        $company = $this->repository->getById($companyCreated->id);
-
-        $this->assertEquals($companyCreated->toArray(), $company);
-
-        $companyCreated->delete();
-
-        $this->expectException(ModelNotFoundException::class);
-        $this->repository->getById($company['id']);
-    }
-
-    public function testDeleteById()
-    {
-        $companyCreated = Company::factory()->create();
-        $this->seeInDatabase('companies', ['id' => $companyCreated->id]);
-
-        $this->repository->deleteById($companyCreated->id);
-        $this->notSeeInDatabase('companies', ['id' => $companyCreated->id]);
-
-        $this->expectException(ModelNotFoundException::class);
-        $this->repository->deleteById($companyCreated->id);
-    }
-
-    public function testCreate()
-    {
-        $this->repository->create([
-            'name' => 'Nubank',
-        ]);
-
-        $this->seeInDatabase('companies', [
-            'name' => 'Nubank',
-        ]);
-
-        $this->expectException(QueryException::class);
-        $this->repository->create([]);
-    }
-
-    public function testUpdate()
-    {
-        $companyCreated = Company::factory()->create([
-            'name' => 'Nubank'
-        ]);
-
-        $this->seeInDatabase('companies', [
-            'id' => $companyCreated->id,
-            'name' => 'Nubank',
-        ]);
-
-        $this->repository->update($companyCreated->id, [
-            'name' => 'Banco Inter',
-        ]);
-
-        $this->seeInDatabase('companies', [
-            'id' => $companyCreated->id,
-            'name' => 'Banco Inter',
-        ]);
-    }
+    protected array $updateData = [
+        'name' => 'Banco Inter'
+    ];
 }
